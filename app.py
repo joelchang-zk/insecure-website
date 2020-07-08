c7d9bfb9505eceb55ba664dacdb5928408a54110
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 11:13:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from __init__ import app
-from waitress import serve
 
 if __name__ == '__main__':
-    # app.run(debug=False, port=5300)
-    serve(app, port = 5300)
+    app.run(debug=False, port=5300)
+    
