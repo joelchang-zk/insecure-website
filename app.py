75690f67cc482133bc40339731efe8a3c2d214bb
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 21:17:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xd2\x04S4\xbc\xce\xe2\x17\xfb\xff\x19C@\xa6e\xc2\xf4\x18\xad\xe8\xc4\xcb'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-app.route("/", methods = ['GET','POST'])
+@app.route('/')
 def index():
-    return render_template('about.html')
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5300)
-    
+    app.run(threaded=True, port=5000)
