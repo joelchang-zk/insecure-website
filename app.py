5db1dda67e67be17b10071d456f68e4fa1d1ad56
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 21:57:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-from ..setup import setUpDB
+from helper.setup import setUpDB
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xd2\x04S4\xbc\xce\xe2\x17\xfb\xff\x19C@\xa6e\xc2\xf4\x18\xad\xe8\xc4\xcb'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-connection = setupDB()
+connection = setUpDB()
 print("DB SET UP")
 @app.route('/')
 def index():
