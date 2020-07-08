1cfe84ee2c2a2d42cff29039301aa7fca9a54b74
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 22:36:49 +0800

    Update setup.py

--- helper/setup.py
+++ helper/setup.py
@@ -10,11 +10,13 @@ import os
 import urllib.parse as up
 import psycopg2
 
-up.uses_netloc.append("postgres")
-url = up.urlparse(os.environ["postgres://hrtaronw:nAzNly4037pRe5h5ercGQYap2wNOah79@ruby.db.elephantsql.com:5432/hrtaronw"])
-conn = psycopg2.connect(database=url.path[1:],
-user=url.username,
-password=url.password,
-host=url.hostname,
-port=url.port
-)
+def setUpDB():
+  up.uses_netloc.append("postgres")
+  url = up.urlparse(os.environ["postgres://hrtaronw:nAzNly4037pRe5h5ercGQYap2wNOah79@ruby.db.elephantsql.com:5432/hrtaronw"])
+  conn = psycopg2.connect(database=url.path[1:],
+  user=url.username,
+  password=url.password,
+  host=url.hostname,
+  port=url.port
+  )
+  return conn
