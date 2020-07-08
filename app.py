87642cfe90702dd68c7eb3e90284fd79c2398dcb
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 22:11:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, request, jsonify
 from helper.setup import setUpDB
+from routes.general import general
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xd2\x04S4\xbc\xce\xe2\x17\xfb\xff\x19C@\xa6e\xc2\xf4\x18\xad\xe8\xc4\xcb'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-connection = setUpDB()
-print("DB SET UP")
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+app.register_blueprint(general.general_blueprint)
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
