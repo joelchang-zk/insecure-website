9e19237dd03ac1f9aa772e8098b73c28796fbe34
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 22:44:26 +0800

    Update setup.py

--- helper/setup.py
+++ helper/setup.py
@@ -4,4 +4,4 @@ import psycopg2
 def setUpDB():
   up.uses_netloc.append("postgres")
   connection = psycopg2.connect(user="hrtaronw", password="nAzNly4037pRe5h5ercGQYap2wNOah79", host="ruby.db.elephantsql.com", port="5432", database="hrtaronw")
-  return conn
+  return connection
