028e302129cda6acb0ea18539e6d68ff903db2f9
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 21:13:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xd2\x04S4\xbc\xce\xe2\x17\xfb\xff\x19C@\xa6e\xc2\xf4\x18\xad\xe8\xc4\xcb'
@@ -7,7 +7,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app.route('/', methods = ['GET','POST'])
 def index():
-    return ('Hello World!')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=False, port=5300)
