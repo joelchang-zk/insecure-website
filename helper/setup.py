0e317a3331bd7f44a2b3cfe334a88e4848e21b95
joelchang-zk <67990607+joelchang-zk@users.noreply.github.com>
2020-07-08 22:32:57 +0800

    Update setup.py

--- helper/setup.py
+++ helper/setup.py
@@ -1,7 +1,20 @@
-import psycopg2
-import os
+# import psycopg2
+# import os
 
-def setUpDB():
-    DATABASE_URL = os.environ['DATABASE_URL']
-    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-    return connection
+# def setUpDB():
+#     DATABASE_URL = os.environ['DATABASE_URL']
+#     connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+#     return connection
+
+import os
+import urllib.parse as up
+import psycopg2
+
+up.uses_netloc.append("postgres")
+url = up.urlparse(os.environ["postgres://hrtaronw:nAzNly4037pRe5h5ercGQYap2wNOah79@ruby.db.elephantsql.com:5432/hrtaronw"])
+conn = psycopg2.connect(database=url.path[1:],
+user=url.username,
+password=url.password,
+host=url.hostname,
+port=url.port
+)
